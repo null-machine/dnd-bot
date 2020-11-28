8de4c62f730025597beb79037052817392a31d8e
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-11-28 10:54:14 -0500

    tracking enabled... hm.

--- Bot.cs
+++ Bot.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DSharpPlus;
+using DSharpPlus.Entities;
 
-namespace DiscordBot
-{
-	class Bot
-	{
+namespace DiscordBot {
+	class Bot {
 
-		// add dynamic commands, e.g. "hey bhot, roll please"
+		static DiscordClient client;
+		static DiscordChannel relay;
+		static string[] names = new string[] { "bot", "bhot" };
+		static string[] hearts = new string[] { ":sparkling_heart:", ":revolving_hearts:" };
+		static Random random = new Random();
+
 		// add thank you responses
+		// truncate roll history to first three
+		// highlight maxes and mins in results
+		// add CheckContext()
+		// make main static instead of the whole ass bot static
 
 		static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
 
-		static Roll TestRoll(string split)
-		{
+		static Roll TestRoll(string split) {
 			string[] splits = split.Split('d');
 			int repeats = 0, size = 0;
 			if (splits.Length != 2) return null;
@@ -23,41 +31,63 @@ namespace DiscordBot
 			return new Roll(repeats, size);
 		}
 
-		static async Task MainAsync()
-		{
-			var discord = new DiscordClient(new DiscordConfiguration()
-			{
-				Token = "NTUxMzc4Nzg4Mjg2NDY0MDAw.XHp1gw.ewM9OUP4IhPvceDj8JBn-sYNk3s",
-				TokenType = TokenType.Bot
-			});
+		static bool CheckCommand(string content) {
+			string[] segments = content.Split(' ');
+			string[] prefixes = new string[] { "roll", "please roll" };
+			string[] triggers = new string[] { "to", "for", "a", "an", "please" };
+			foreach (string prefix in prefixes) if (content.StartsWith(prefix)) return true;
+			foreach (string trigger in triggers) if (content.Contains($"roll {trigger}")) return true;
+			if (content.Contains("roll")) {
+				foreach (string name in names) {
+					if (content.Contains($" {name}") || content.StartsWith(name)) {
+						return true;
+					}
+				}
+			}
+			if (TestRoll(segments[0]) != null) return true;
+			return false;
+		}
 
-			discord.MessageCreated += async e =>
-			{
-				//Console.WriteLine(e.Message.Content.ToLower());
-				if (e.Message.Content.ToLower().StartsWith("🗡")) await e.Message.RespondAsync(":dagger:");
-				if (e.Message.Content.ToLower().StartsWith("boop")) await e.Message.RespondAsync(":sparkling_heart:");
-				if (e.Message.Content.ToLower().StartsWith("nya")) await e.Message.RespondAsync("https://cdn.discordapp.com/emojis/781352087123787797.png");
-				if (e.Message.Content.ToLower().StartsWith("roll"))
-				{
+		static bool CheckBoop(string content) {
+			if (content.StartsWith("boop")) return true;
+			foreach (string name in names) if (content.Contains("boop") && (content.EndsWith("!") || content.Contains(name))) return true;
+			if (content.Contains("a boop") || content.Contains("boop for")) return true;
+			return false;
+		}
 
-					List<string> parameters = new List<string>(e.Message.Content.Split(' '));
+		static async Task MainAsync() {
+			Console.WriteLine("bot online!!");
+			DiscordConfiguration config = new DiscordConfiguration() {
+				Token = "NTUxMzc4Nzg4Mjg2NDY0MDAw.XHp1gw.ewM9OUP4IhPvceDj8JBn-sYNk3s"
+			};
+			client = new DiscordClient(config);
+			relay = await client.GetChannelAsync(782245881625182269);
+
+			client.MessageCreated += async e => {
+				if (e.Channel == relay) return;
+				string logString = $"`{e.Author.Username}#{e.Author.Discriminator} ";
+				logString += $"{e.Channel?.Name} " ?? "";
+				logString += $"{e.Guild?.Name} " ?? "";
+				logString = $"{logString.Trim()}`\n{e.Message.Content}";
+				relay.SendMessageAsync(logString);
+
+				string content = e.Message.Content.ToLower();
+				string[] segments = e.Message.Content.Split(' ');
+				for (int i = 0; i < segments.Length; i++) segments[i] = new string(segments[i].Where(j => !char.IsPunctuation(j)).ToArray());
+				if (CheckBoop(content)) await e.Message.RespondAsync(hearts[random.Next(hearts.Length)]);
+				if (content.StartsWith("nya") || content.Contains(" nya")) await e.Message.RespondAsync("https://cdn.discordapp.com/emojis/781352087123787797.png");
+				if (CheckCommand(content)) {
 					int mod = 0, repeats = 0, parse;
 					List<Roll> rolls = new List<Roll>();
 					Roll roll;
-					foreach (string param in parameters)
-					{
-						if (int.TryParse(param, out parse))
-						{
+					foreach (string param in segments) {
+						if (int.TryParse(param, out parse)) {
 							mod += parse;
-						}
-						else if (param.StartsWith("r"))
-						{
+						} else if (param.StartsWith("r")) {
 							if (int.TryParse(param[1..], out parse)) repeats += parse;
 							else if (param[1..].Equals("")) repeats += 2;
-						}
-						else if (param.Equals("adv") || param.Equals("dis")) repeats += 2;
-						else
-						{
+						} else if (param.Equals("adv") || param.Equals("dis")) repeats += 2;
+						else {
 							roll = TestRoll(param);
 							if (roll != null) rolls.Add(roll);
 						}
@@ -69,10 +99,8 @@ namespace DiscordBot
 					string[] rollStr = new string[repeats];
 					int[] results = new int[repeats];
 					bool critical = false;
-					for (int i = 0; i < repeats; i++)
-					{
-						for (int j = 0; j < rolls.Count; j++)
-						{
+					for (int i = 0; i < repeats; i++) {
+						for (int j = 0; j < rolls.Count; j++) {
 							if (rolls[j].Critical) critical = true;
 							rollStr[i] += $"{rolls[j].input} ";
 							if (j != rolls.Count - 1) rollStr[i] += "+ ";
@@ -82,15 +110,13 @@ namespace DiscordBot
 					}
 					string message = "";
 					string resultStr = results.Length > 1 ? "**Results:** " : "**Result:** ";
-					for (int i = 0; i < repeats; i++)
-					{
+					for (int i = 0; i < repeats; i++) {
 						if (mod < 0) rollStr[i] += $"- {-mod}";
 						else if (mod > 0) rollStr[i] += $"+ {mod}";
 						results[i] += mod;
 						message += $"**Roll:** {rollStr[i]}\n";
 						resultStr += results[i];
-						if (i != repeats - 1)
-						{
+						if (i != repeats - 1) {
 							resultStr += ", ";
 						}
 					}
@@ -102,7 +128,7 @@ namespace DiscordBot
 
 			};
 
-			await discord.ConnectAsync();
+			await client.ConnectAsync();
 			await Task.Delay(-1);
 		}
 	}
